0447536967457037c6e5080ed40336b0ded1ae47
Benjamin Götz <benjamin.goetz@rybos.de>
2023-04-14 15:50:32 +0200

    Splitted vendors into datatypes for overview

--- cve_ds.py
+++ cve_ds.py
@@ -14,7 +14,9 @@ class CVESearch:
         '''Returns all vendors stored in the databases'''
 
         request = requests.get(self.url + "browse", timeout=5).json()
-        pprint.pprint(request)
+        pprint.pprint(request.keys())
+        print(type(request)) #Dict
+        print(request.get('vendor')[10]) #List
 
     def get_products_per_vendor(self):
         '''Get all the products associated to a vendor with known CVEs'''
@@ -36,12 +38,14 @@ class CVESearch:
 
         cve = input("What CVE are you searching for?")
         request = requests.get(self.url + "cve/" + cve, timeout=5).json()
-        pprint.pprint(request)
+        #pprint.pprint(request.get('id'), request.get('summary'))
+        print(request.get('id'))
+        print(request.get('summary'))
 
     def db_info(self):
         '''Get infos about the used databases of the API'''
 
-        request = requests.get(self.url + 'dbInfo', timeout=5).json()
+        request = requests.get(self.url + 'dbinfo', timeout=5).json()
         pprint.pprint(request)
 
     def latest_30_cves(self):
@@ -49,16 +53,21 @@ class CVESearch:
 
         request = requests.get(self.url + 'last', timeout=5).json()
         pprint.pprint(request)
+        for vul in request:
+            print(vul.get('id'))
+            print(vul.get('summary'))
+            print(vul.keys())
 
 
 test = CVESearch()
-#test.get_vendors()
+test.get_vendors()
 #test.get_products_per_vendor()
 #test.get_vulnerabilities_per_product()
 #test.get_cve_info()
 #test.db_info()
-test.latest_30_cves()
+#test.latest_30_cves()
 
+'''CVE-ID und Titel ausgeben'''
 
 # OpenCVE offers a public API to retrieve DATA:
 # Documentation can be found at: https://docs.opencve.io/
