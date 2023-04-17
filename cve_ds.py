0c24dac02310141b92a45f3e1383401999c30afa
Benjamin Götz <benjamin.goetz@rybos.de>
2023-04-17 18:12:02 +0200

    Added function for severity and score

--- cve_ds.py
+++ cve_ds.py
@@ -2,62 +2,61 @@
 import pprint
 import requests
 
-class CVESearch:
-    '''
-    Class for CVE-Search etc.
-    '''
+URL = "https://cvepremium.circl.lu/api/"
 
-    def __init__(self) -> None:
-        self.url = "https://cvepremium.circl.lu/api/"
+def get_vendors():
+    '''Returns all vendors stored in the databases'''
 
-    def get_vendors(self):
-        '''Returns all vendors stored in the databases'''
+    request = requests.get(URL + "browse", timeout=50).json()
+    pprint.pprint(request.keys())
+    print(type(request)) #Dict
+    print(request.get('vendor')[10]) #List
 
-        request = requests.get(self.url + "browse", timeout=50).json()
-        pprint.pprint(request.keys())
-        print(type(request)) #Dict
-        print(request.get('vendor')[10]) #List
+def get_products_per_vendor():
+    '''Get all the products associated to a vendor with known CVEs'''
 
-    def get_products_per_vendor(self):
-        '''Get all the products associated to a vendor with known CVEs'''
+    vendor = input("For which VENDOR do you want to know the products?")
+    request = requests.get(URL + "browse/" + vendor, timeout=50).json()
+    pprint.pprint(request)
 
-        vendor = input("For which VENDOR do you want to know the products?")
-        request = requests.get(self.url + "browse/" + vendor, timeout=50).json()
-        pprint.pprint(request)
+def get_vulnerabilities_per_product():
+    '''Get all the vulnerabilities associated to a certain vendor and a certain product'''
 
-    def get_vulnerabilities_per_product(self):
-        '''Get all the vulnerabilities associated to a certain vendor and a certain product'''
+    vendor = input("Which VENDOR?").lower()
+    product = input("Which PRODUCT?").lower()
+    request = requests.get(URL + "search/" + vendor + "/" + product, timeout=5).json()
+    pprint.pprint(request)
 
-        vendor = input("Which VENDOR?").lower()
-        product = input("Which PRODUCT?").lower()
-        request = requests.get(self.url + "search/" + vendor + "/" + product, timeout=5).json()
-        pprint.pprint(request)
+def get_cve_info(cvenumber):
+    '''Get infos about a certain CVE'''
 
-    def get_cve_info(self, cvenumber):
-        '''Get infos about a certain CVE'''
+    #cve = input("What CVE are you searching for?")
+    request = requests.get(URL + "cve/" + cvenumber, timeout=50).json()
+    #pprint.pprint(request.get('id'), request.get('summary'))
+    # print(request.get('id'))
+    # print(request.get('summary'))
+    return request
 
-        #cve = input("What CVE are you searching for?")
-        request = requests.get(self.url + "cve/" + cvenumber, timeout=50).json()
-        #pprint.pprint(request.get('id'), request.get('summary'))
-        # print(request.get('id'))
-        # print(request.get('summary'))
-        return request
+def get_cve_severity_and_score(cvenumber):
+    '''Return the severity and the cvss of a CVE'''
+    request = requests.get(URL + "cve/" + cvenumber, timeout=50).json()
+    return request["typical_severity"], request["cvss3"]
 
-    def db_info(self):
-        '''Get infos about the used databases of the API'''
+def db_info():
+    '''Get infos about the used databases of the API'''
 
-        request = requests.get(self.url + 'dbinfo', timeout=50).json()
-        pprint.pprint(request)
+    request = requests.get(URL + 'dbinfo', timeout=50).json()
+    pprint.pprint(request)
 
-    def latest_30_cves(self):
-        '''Print newest 30 CVEs which were uploaded to the DBs'''
+def latest_30_cves():
+    '''Print newest 30 CVEs which were uploaded to the DBs'''
 
-        request = requests.get(self.url + 'last', timeout=50).json()
-        pprint.pprint(request)
-        for vul in request:
-            print(vul.get('id'))
-            print(vul.get('summary'))
-            print(vul.keys())
+    request = requests.get(URL + 'last', timeout=50).json()
+    pprint.pprint(request)
+    for vul in request:
+        print(vul.get('id'))
+        print(vul.get('summary'))
+        print(vul.keys())
 
 
 # test = CVESearch()
