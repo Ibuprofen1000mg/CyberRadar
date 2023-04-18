06c9da8bfee5dbf4d5415dd0878b6fd3b4aa57cd
jessekuhn <80405983+jessekuhn@users.noreply.github.com>
2023-04-18 18:12:24 +0200

    add: diagramm severity

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -3,23 +3,10 @@ from dash import dcc
 from dash import html
 import pandas as pd
 from collections import Counter
+import plotly.graph_objs as go
+
+import cve_ds
 from Tweety import TwitterCVE
-
-# data = pd.read_csv("./avocado.csv")
-# data = data.query("type == 'conventional' and region == 'Albany'")
-# data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
-# data.sort_values("Date", inplace=True)
-
-NewTwitter = TwitterCVE()
-retrieveLast100 = NewTwitter.get_tweets("#cve -from:RedPacketSec", 100)
-last100Tweets = NewTwitter.get_cve_in_tweets(retrieveLast100)
-
-print(type(Counter(last100Tweets)))
-
-data = {'Name': ['John', 'Amy', 'Peter', 'Jane'],
-        'Age': [23, 45, 31, 28],
-        'City': ['New York', 'London', 'Paris', 'Sydney']}
-df = pd.DataFrame(data)
 
 external_stylesheets = [
     {
@@ -29,17 +16,45 @@ external_stylesheets = [
     },
 ]
 
+NewTwitter = TwitterCVE()#
+last100Tweets = NewTwitter.get_cve_in_tweets(NewTwitter.get_tweets("#cve -from:RedPacketSec", 100))
+
+unfilterd_cve = list(Counter(last100Tweets).keys())
+filtered_score = []
+filtered_severity = []
+labels = ['MEDIUM', 'HIGH', 'N/A', 'CRITICAL']
+values = [0, 0, 0, 0]
+
+for x in unfilterd_cve:
+    cve_info = cve_ds.get_cve_info2(x)
+    try:
+        filtered_score.append(cve_info[1]['cvssV3_score'])
+        filtered_severity.append(cve_info[1]['severity'])
+        if (cve_info[1]['severity']=='MEDIUM'):
+            values[0] += 1
+        elif (cve_info[1]['severity']=='HIGH'):
+            values[1] += 1
+        elif (cve_info[1]['severity']=='N/A'):
+            values[2] += 1
+        elif (cve_info[1]['severity']=='CRITICAL'):
+            values[3] += 1
+        print(x)
+    except KeyError: continue
+
+fig = go.Figure(data=[go.Pie(labels=labels, values=values)], layout={
+    'title': 'Severity Distribution'
+})
+
 dash.register_page(__name__)
 
 layout = html.Div(
     children=[
-        
         html.Div(
-            style={'display': 'flex', 'flex-wrap': 'wrap',},
+            style={'display': 'flex', 'flexWrap': 'wrap',},
             children=[
                 html.Div(
                     className="card",
-                    style= {'margin-right': '10px', 'flex': '1'},
+                    style= {'marginRight': '10px', 'flex': '1'},
                     children=dcc.Graph(
                         id="numbers-chart",
                         config={"displayModeBar": False},
@@ -60,6 +75,15 @@ layout = html.Div(
                                 'colorway': ['#E12D39']
                             }
                         },
+                    ),
+                ),
+                html.Div(
+                    className="card",
+                    style= {'marginRight': '10px', 'flex': '1'},
+                    children=
+                    dcc.Graph(
+                        id='pie-chart',
+                        figure=fig
                     ),
                 ),
             ],
