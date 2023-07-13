97b05e160b75556c3e1dfe9ec8169c2515d5e828
TheBillyTheHeide <billyheide26@gmail.com>
2023-07-13 21:27:20 +0200

    Added Comments to WebCrawl

--- WebCrawl.py
+++ WebCrawl.py
@@ -1,30 +1,39 @@
-'''File for retrieving data from RSS feeds of different websites'''
+"""File for retrieving data from RSS feeds of different websites"""
 import re
 import requests
 from bs4 import BeautifulSoup
 from collections import Counter
 
-
+"""Global Variables used in this class"""
 WEBSITES_FILE_NAME = "./Textfiles/Websites.txt"
 header = {
     'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
 }
-
 cve_array = []
 
-def get_cve_in_Website(string_to_search):
-    '''Returns CVE REGEX found in an array of tweets'''
+def get_cve_in_Website(string_to_search:str) -> list:
+    """Returns CVE REGEX found in an array of tweets
+
+    Args:
+        string_to_search (str): String to search for CVE Regex Pattern
+
+    Returns:
+        _type_: List containing found CVE given a string
+    """
+    print(type(re.findall('CVE-\\d{4}-\\d{4,7}', string_to_search)))
     return re.findall('CVE-\\d{4}-\\d{4,7}', string_to_search)
 
-def parse_websites():
-    '''Parse website(s) in RSS-Websites file'''
+def parse_websites() -> list:
+    """Parse website(s) in RSS-Websites file
+
+    Returns:
+        list: List containing all CVE Patterns from a list of Strings
+    """
     with open(WEBSITES_FILE_NAME, "r", encoding="utf-8") as web_file:
         for url in web_file.readlines():
             try:
                 x = requests.get(url, headers=header)
                 soup = BeautifulSoup(x.content.decode('utf-8'),  "html.parser")
-                #print(soup.text)
-                #print((soup.text))
                 x = (get_cve_in_Website(soup.text.upper()))
                 if x:
                     cve_array.extend(x)
