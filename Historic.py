912a6f9d520dcbf39b0834f4527ebebd74e583d9
DroneNerdDE <Benjamin.goetz@rybos.de>
2023-07-15 13:04:26 +0200

    Dokuvorlage geben & pylint errors minimieren

--- Historic.py
+++ Historic.py
@@ -1,12 +1,14 @@
+"""File for the generation of the historic CVE data"""
+import datetime
 import requests
 from bs4 import BeautifulSoup
-import datetime
+
 
 today = datetime.date.today()
 datasets = []
-url = "https://www.cvedetails.com/browse-by-date.php"
+URL = "https://www.cvedetails.com/browse-by-date.php"
 
-response = requests.get(url)
+response = requests.get(URL)
 
 soup = BeautifulSoup(response.content.decode('utf-8'),  "html.parser")
 data = soup.find_all("td", attrs={"class":"num"})
@@ -26,5 +28,10 @@ def dates_array() -> list:
     return datasets
 
 def data_array():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
     strip_list = [ x.replace('\t', '').replace('\n', '') for x in cve_data ]
     return strip_list
